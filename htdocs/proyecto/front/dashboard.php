1cae45a4c6ded935ae69a845f4dfbb1cc9edd434
IsraCard <isracard66@gmail.com>
2024-05-10 13:29:16 -0600

    Creado el espacio para dashboard. El estilo css fue movido de /root/ a /root/styles

---
+++ htdocs/proyecto/front/dashboard.php
@@ -0,0 +1,30 @@
+<?php
+include("back/jsonWriter.php");
+include("back/jsonReader.php");
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="styles/dashboard.css">
+</head>
+<body>
+    <header><!--Hacer menu principal--></header>
+<div class="catalogoContainer">
+    <table class="catalogo">
+        <tr>
+            <th class="catalogoHeader">ID</th>
+            <th class="catalogoHeader">Modelo</th>
+            <th class="catalogoHeader">Ubicacion</th>
+        </tr>
+        <?php
+        
+        ?>
+    </table>
+</div>
+    
+</body>
+</html>
