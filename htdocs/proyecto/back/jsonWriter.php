aae81ad3f8927cde31724fc31ca7124495787436
IsraCard <isracard66@gmail.com>
2024-05-13 17:10:50 -0600

    Añadido dashboard. Funciona al 50%

--- htdocs/proyecto/back/jsonWriter.php
+++ htdocs/proyecto/back/jsonWriter.php
@@ -1,15 +1,17 @@
 <?php
 //Para escribir un json, se requieren varios parametros ordenado de la forma($IDCliente,$Nombre,$Correo,$Contenido)
 function write($IDCliente,$Nombre,$Correo,$Contenido){
+    $ini = random_int(100,999);
+    $issueID = $IDCliente.$ini;
     $array = [
+        "IssueID" => "$issueID",
         "IDCliente" => "$IDCliente",
         "Nombre" => "$Nombre",
         "Correo" => "$Correo",
         "Contenido" => "$Contenido"
     ];
-    $ini = random_int(100,999);
     $json = json_encode($array,JSON_PRETTY_PRINT);
-    $path = "JSON/".$array["IDCliente"].$ini.".json";
+    $path = "JSON/".$issueID.".json";
     file_put_contents($path,$json);
 }
 
--- htdocs/proyecto/front/dashboard.php
+++ htdocs/proyecto/front/dashboard.php
@@ -1,6 +1,14 @@
 <?php
-include("back/jsonWriter.php");
-include("back/jsonReader.php");
+include("../back/jsonReader.php");
+include("../back/dataBaseConnection.php");
+function reloadList() {
+    $dirArray = array_diff(scandir("../JSON"),array('.','..'));
+    foreach($dirArray as $element){
+        $issuePath = "../JSON/".$element;
+        $issue = readOne($issuePath);
+        echo "<li><button>".$issue["IssueID"]."</button></li>";
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -8,23 +16,38 @@ include("back/jsonReader.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
-    <link rel="stylesheet" href="styles/dashboard.css">
+    <title>Centro de atención a clientes</title>
+    <link rel="stylesheet" href="../front/styles/dashboard.css">
 </head>
 <body>
-    <header><!--Hacer menu principal--></header>
-<div class="catalogoContainer">
-    <table class="catalogo">
-        <tr>
-            <th class="catalogoHeader">ID</th>
-            <th class="catalogoHeader">Modelo</th>
-            <th class="catalogoHeader">Ubicacion</th>
-        </tr>
-        <?php
-        
-        ?>
-    </table>
+<div class="AtencionContainer">
+    <div class="Catalogo">
+        <table>
+            <caption>Catálogo</caption>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Modelo</th>
+                    <th>Precio</th>
+                    <th>Ubicación</th>
+                </tr>
+                <tbody>
+                    <?php
+                    reloadCatalog($catalogoBD);
+                    ?>
+                </tbody>
+            </thead>
+        </table>
+    </div>
 </div>
-    
+<div class="AtencionContainer">
+    <div class="lista">
+        <ul>
+            <?php
+            reloadList();
+            ?>
+        </ul>
+    </div>
+</div>
 </body>
 </html>
