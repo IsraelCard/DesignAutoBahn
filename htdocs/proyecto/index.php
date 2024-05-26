2725c87ba3156864520795964e9378cc0748d265
AlonsoHC04 <Alonsohcxd@gmail.com>
2024-05-25 22:37:10 -0600

    contenido de la pagina principal

--- htdocs/proyecto/index.php
+++ htdocs/proyecto/index.php
@@ -6,10 +6,41 @@
     <title>FastCar</title>
     <link rel="stylesheet" href="styles/index.css">
     <link rel="stylesheet" href="styles/bootstrap.min.css">
+    <style>
+        
+        #video-container {
+            width: 80%; 
+            margin: 20px auto; 
+            overflow: hidden; 
+        }
+
+        video {
+            width: 100%; 
+            height: auto; 
+        }
+
+        
+        #bienvenido {
+            text-align: center; 
+            font-size: 46px; 
+            font-weight: bold; 
+        }
+    </style>
 </head>
 <body>
     <?php include("header.php");?>
-    Bienvenido
+
+    
+    <div id="video-container">
+        <video controls autoplay muted loop>
+            <source src="assets/images/featured-cars/carvideo.mp4" type="video/mp4">
+            Tu navegador no admite el elemento de video.
+        </video>
+    </div>
+
+    
+    <p id="bienvenido">Bienvenido a Autostock</p>
+
     <?php include('footer.php') ?>
 </body>
 </html>
